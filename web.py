6a5edb90fad85c50296c604c86a453ff0e5034a1
Ds123-wq <64480480+Ds123-wq@users.noreply.github.com>
2020-12-12 12:18:56 +0530

    ansible-playbook

---
+++ web.py
@@ -0,0 +1,62 @@
+- hosts: all
+  vars:
+    - mount_point: "/dvd"
+    - http_port: "8080"
+    - document_root: "/var/www/deepak"
+    
+  tasks:
+  - name: "create mountpoint"
+    file:
+      path: "{{ mount_point }}"
+      state: directory
+  - name: "mount the dvd"
+    mount:
+      path: "{{ mount_point }}"
+      src: "/dev/cdrom"
+      state: mounted
+      fstype: "iso9660"
+  - name: "configure yum"
+    yum_repository:
+      name: "AppStream"
+      description: "configure AppStream"
+      baseurl: "{{ mount_point }}/AppStream"
+      gpgcheck: no
+  - name: "configure yum"
+    yum_repository:
+      name: "BaseOS"
+      description: "configure BaseOS"
+      baseurl: "{{ mount_point }}/BaseOS"
+      gpgcheck: no
+
+  - name: "Install httpd"
+    package:
+      name: "httpd"
+  - name: "create document_root"
+
+    file:
+      path: "{{ document_root }}"
+      state: directory
+  - name: "copy the webpage"
+    copy:
+      dest: "{{ document_root }}/index.html"
+      src: "webpage.html"
+
+  - name: "copy the conf file"
+    template:
+      src: "lw.conf.j2"
+      dest: "/etc/httpd/conf.d/lw.conf"
+    notify: Restart service
+
+
+  - name: "create firewall rule"
+    firewalld:
+      port: "{{ http_port }}/tcp"
+      immediate: yes
+      state: enabled
+
+  handlers:
+  - name: "Restart service"
+    service:
+      name: "httpd"
+      state: restarted
+
